07dacae4d86114135a452f9ae2d12700a2a3ed3b
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-19 14:54:56 -0700

    adding array of struct to header and printf files

--- _printf.c
+++ _printf.c
@@ -2,20 +2,25 @@
 
 int _printf(const char *format, ...)
 {
-    va_list List;
-    int i = 0, j;
+	format[] = {
+		{'i', print_str},
+		{'c', print_char},
+		{NULL, NULL}
+	};
 
-    va_start(List, format);
-    while (format != NULL && format[i] != '\0')
-    {
-        
-        if (format[i] == '%')
-        {
+	va_list List;
+	int i = 0, j;
+	va_start(List, format);
 
-        }
-        _putchar(format[i]);
-        i++;
-    }
+	while (format != NULL && format[i] != '\0')
+	{
 
-    va_end(List);
+		if (format[i] == '%')
+		{
+			if ((format[i + 1]) == 's')
+		}
+		_putchar(format[i]);
+		i++;
+	}
+	va_end(List);
 }
--- main.h
+++ main.h
@@ -4,6 +4,12 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+typedef struct format
+{
+	char form;
+	void (*f)(va_list);
+}format;
+
 int _printf(const char *format, ...);
 int _putchar(int s);
 void print_str(char *c);
