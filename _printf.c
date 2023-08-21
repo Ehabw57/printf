cdaf90840d680fdbff5ac0b33c15433eb318961e
MOHAMedelhennawy <el7ennawy@gmail.com>
2023-08-21 07:13:46 -0700

    Fixed number of lines printf file

--- _printf.c
+++ _printf.c
@@ -3,32 +3,22 @@
 
 int _printf(const char *format, ...)
 {
-	form arr[] = {
-		{'s', print_str},
-		{'c', print_char},
-		{'i', print_int},
-		{'d', print_INT},
-		{'d', print_int},
-		{'%', print_percent},
-		{'\0', NULL}};
-	int i = 0, j = 0, len = 0, sum = 0;
+	form arr[] = {{'s', print_str},	{'c', print_char}, {'i', print_int},
+		{'d', print_INT}, {'d', print_int}, {'%', print_percent}, {'\0', NULL}};
+	int i = -1, j = 0, len = 0, sum = 0;
 	va_list List;
 
 	va_start(List, format);
 	if (format == NULL || (format[0] == '%' && !format[1]))
 		return (-1);
-
 	if (format[0] == '%' && format[i] == ' ' && !format[2])
 		return (-1);
-	while (format != NULL && format[i] != '\0')
+	while (format != NULL && format[++i] != '\0')
 	{
-
 		if (format[i] == '%')
 		{
-
-			j = 0;
-			i++;
-			while (arr[j].f != '\0')
+			j = -1;
+			while (arr[++j].f != '\0')
 			{
 				if ((format[i]) == arr[j].f)
 				{
@@ -36,14 +26,11 @@ int _printf(const char *format, ...)
 					sum += 2;
 					len += arr[j].fun(List);
 				}
-
-				j++;
 			}
 		}
 		else
 		{
-			_putchar(format[i]);
-			i++;
+			_putchar(format[i++]);
 		}
 	}
 	va_end(List);
