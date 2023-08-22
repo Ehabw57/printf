b8b4adb44c110012427943af40fcd64a1a9e29d6
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-22 11:10:15 +0300

    matched betty code and doc style
    Added the main.h header file

--- search.c
+++ search.c
@@ -1,6 +1,14 @@
+#include "main.h"
+/**
+ * search_f - searches in side the given pointer fo the char f
+ * @f: the char to start searching for
+ * @c: pointer to string of the sourc chars
+ * Return: the index of c that that f located in or -1 if failed
+*/
 int search_f(char f, char *c)
 {
 	int i;
+
 	for (i = 0; c[i] != '\0'; i++)
 	{
 		if (c[i] == f)
