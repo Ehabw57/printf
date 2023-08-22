149ab2e3f670f928ef4c3c477db97300b414e062
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-22 10:50:45 +0300

    deleted uncceray files and matched betty codig style

--- print_char.c
+++ print_char.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * print_char - prints a simple char on the stops using _putchar
+ * @arg: va_list parmeter used to locate the char to be print
+ * Return: the lenth of the char that has been printed
+ */
 int print_char(va_list arg)
 {
 	char c = va_arg(arg, int);
